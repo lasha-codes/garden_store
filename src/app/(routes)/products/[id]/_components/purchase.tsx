89e8e70d3828df1324cbe5bdc2ffe3356b6d3e43
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-11-02 14:01:14 +0400

    basic qty decrement and increment functionality

--- src/app/(routes)/products/[id]/_components/purchase.tsx
+++ src/app/(routes)/products/[id]/_components/purchase.tsx
@@ -24,6 +24,23 @@ const Purchase = ({
       }
     }
   }
+
+  const decrementQty = () => {
+    if (Number(qty) === 1) {
+      return
+    } else {
+      setQty((prev) => (Number(prev) - 1).toString())
+    }
+  }
+
+  const incrementQty = () => {
+    if (Number(qty) === product.qty) {
+      return
+    } else {
+      setQty((prev) => (Number(prev) + 1).toString())
+    }
+  }
+
   return (
     <div className='border rounded-xl flex flex-col items-start p-3.5 gap-3 min-w-[310px] max-w-[310px] max-xl:min-w-[60%] max-xl:max-w-[60%] max-md:min-w-full max-md:max-w-full'>
       <div className='flex items-center gap-0.5 text-lg font-poppins font-medium'>
@@ -33,7 +50,12 @@ const Purchase = ({
       <div className='flex flex-col items-start gap-3 w-full'>
         <div className='flex items-center gap-2'>
           <div className='w-[80px] h-[40px] flex items-center border font-poppins text-gray-500'>
-            <button className='w-full h-full border-r'>-</button>
+            <button
+              onClick={decrementQty}
+              className='w-full h-full border-r hover:bg-main transition-all duration-300 ease-linear hover:text-white'
+            >
+              -
+            </button>
             <input
               onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
                 if (!Number.isInteger(Number(e.target.value))) {
@@ -44,7 +66,12 @@ const Purchase = ({
               value={qty}
               className='w-full h-full outline-none text-center'
             />
-            <button className='w-full h-full border-l'>+</button>
+            <button
+              onClick={incrementQty}
+              className='w-full h-full border-l hover:bg-main transition-all duration-300 ease-linear hover:text-white'
+            >
+              +
+            </button>
           </div>
           <button className='text-sm bg-main hover:bg-main/90 transition-all duration-200 ease-linear h-[40px] text-white px-3'>
             {language === 'geo' ? 'კალათაში დამატება' : 'Add to cart'}
